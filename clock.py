ff211957266d4ab3fa50508e0f335b667f7ec5a6
Saikumo <saikumo11@gmail.com>
2023-09-18 16:48:49 +0800

    change path

--- clock.py
+++ clock.py
@@ -1,15 +1,14 @@
 import subprocess
 import time
 
+command1 = "touch /content/Bert-VITS2/logs/e.pth"
+result = subprocess.run(command1, shell=True)
+
 while True:
     # 执行命令行命令
-    command1 = "touch /content/Bert-VITS2/logs/d.pth"
-    result = subprocess.run(command1, shell=True)
-
-
     command2 = "cp -r /content/Bert-VITS2/logs /content/drive/MyDrive/Bert-VITS2/model"
     result = subprocess.run(command2, shell=True)
 
 
     # 等待一个小时
-    time.sleep(3600)
+    time.sleep(60)
